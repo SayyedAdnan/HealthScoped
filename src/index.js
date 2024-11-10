b14fa869efa98b2f1a4f1021041ad085a83dbdad
SayyedAdnan <adnan.a.sayyed@gmail.com>
2024-11-10 12:33:08 -0500

    update3

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
-import ReactDOM from "react-dom/client";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
